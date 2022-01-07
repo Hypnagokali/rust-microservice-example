d5a114e550b5c5c063461b5e6d1791ac0c83f38d
Stefan Simon <stefan.simon@xenadu.de>
2022-01-07 13:54:23 +0100

    InMemoryDataSource::save distinguishes between update and insert

--- src/data/user.rs
+++ src/data/user.rs
@@ -18,28 +18,53 @@ pub struct InMemoryDataSource<T> {
 }
 
 pub trait BasicRepository<T> {
-    fn find_by_id(&self, id: u64) -> Result<T, &'static str>;
-    fn save(&self, data: &T) -> Result<T, &'static str>;
+    fn find_by_id(&self, id: u64) -> Result<T, String>;
+    fn save(&self, data: &T) -> Result<T, String>;
 }
 
 
 impl BasicRepository<UserData> for InMemoryDataSource<UserData> {
-    fn find_by_id(&self, id: u64) -> Result<UserData, &'static str> {
+    fn find_by_id(&self, id: u64) -> Result<UserData, String> {
         let user_data = self.data_source.lock().unwrap();
 
-        if user_data.contains(id as usize) {
-            Ok(user_data[id as usize])
+        if id < 1 {
+            return Err(String::from("ID < 1"));  
+        }
+
+        let id_in_slab = id - 1;
+
+        if user_data.contains(id_in_slab as usize) {
+            Ok(user_data[id_in_slab as usize])
         } else {
-            Err("User not found")
+            Err(format!("User with ID = {} not found", id))
         }
     }
 
-    fn save(&self, user_data: &UserData) -> Result<UserData, &'static str> {
+    fn save(&self, user_data: &UserData) -> Result<UserData, String> {
         let mut saved_user = user_data.clone();
-        let user_id = self.data_source.lock().unwrap().insert(saved_user);
-        saved_user.id = user_id as u64;
 
-        Ok(saved_user)
+        let mut locked_data_source = self.data_source.lock().unwrap();
+
+        if saved_user.id > 0 {
+            // update user
+            let update_user = locked_data_source.get_mut((user_data.id - 1) as usize);
+
+            if let Some(user) = update_user {
+                *user = saved_user;
+    
+                Ok(saved_user)
+            } else {
+                Err(format!("User not found ID = {}", saved_user.id))
+            }
+        } else {
+            // new user
+            let user_id = locked_data_source.insert(saved_user);
+
+            let updated_saved_user = locked_data_source.get_mut(user_id).unwrap();
+            updated_saved_user.id = (user_id + 1) as u64;
+
+            Ok(*updated_saved_user)
+        }
     }
 }
 
